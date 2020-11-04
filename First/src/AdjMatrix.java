7330d5d4a626281df6f135690f8a831b151039e7
ginguan <yguan074@uottawa.ca>
2020-11-03 23:30:44 -0500

    update

--- First/src/AdjMatrix.java
+++ First/src/AdjMatrix.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AdjMatrix {
@@ -14,21 +15,58 @@ public class AdjMatrix {
         try(Scanner scanner = new Scanner(file)){
 
             V = scanner.nextInt();
+            if(V < 0) throw new IllegalArgumentException("V must be non-negative");
             adj = new int[V][V];
 
             E = scanner.nextInt();
+            if(E < 0) throw new IllegalArgumentException("E must be non-negative");
             for(int i = 0; i < E; i ++){
                 int a = scanner.nextInt();
+                validateVertex(a);
                 int b = scanner.nextInt();
+                validateVertex(b);
+                //self loop/parallel detection
+                if(a == b) throw new IllegalArgumentException("Self Loop is Detected!");
+                if(adj[a][b] == 1) throw new IllegalArgumentException("Parallel Edges are Detected!");
                 adj[a][b] = 1;
                 adj[b][a] = 1;
             }
         }
         catch(IOException e){
             e.printStackTrace();
-        }
+        } }
+    private void validateVertex(int v){
+        if(v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + "is invalid");
+    }
+    //
+    public int V(){
+        return V;
     }
 
+    public int E(){
+        return E;
+    }
+    public boolean hasEdge(int v, int w){
+        validateVertex(v);
+        validateVertex(w);
+        return adj[v][w] == 1;
+    }
+
+    //port
+    //return neighbor of v
+    public ArrayList<Integer> adj(int v){
+        validateVertex(v);
+        ArrayList<Integer> neighbor = new ArrayList<>();
+        for(int i = 0; i < V; i ++)
+            if(adj[v][i] == 1)
+                neighbor.add(i);
+        return neighbor;
+    }
+
+    public int degree(int v){
+        return adj(v).size();
+    }
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
